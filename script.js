f6737ed45e8167a339379381c9dd6b2cbaf8fd2f
known1417 <6789randomstuff@gmail.com>
2024-07-13 23:10:02 +0530

    flag

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "To My Closest";
+
+desc.innerText = "Today, I woke up with my heart racing and with a smile that does not erase, because I remembered that it is the birthday of a beautiful person. Happy Birthday, akshara!:x";
+
+sname.innerText = "sunny\n17.07.2024";
