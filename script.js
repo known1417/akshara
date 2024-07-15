82d1f072f3df5293475a828f7efe89bfeabca372
known1417 <6789randomstuff@gmail.com>
2024-07-15 15:14:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ const sname = document.getElementById("name")
 
 title.innerText = "To My Closest";
 
-desc.innerText = "Today, I woke up with my heart racing and with a smile that does not erase, because I remembered that it is the birthday of a beautiful person. Happy Birthday, akshara!:x";
+desc.innerText = "Today, I woke up with my heart racing and with a smile that does not erase, because I remembered that it is the birthday of a beautiful person. Happy Birthday, akshara mahi!:x";
 
 sname.innerText = "sunny\n17.07.2024";
